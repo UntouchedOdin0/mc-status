28bbe2e768cfe5a4b555e9b8447d86e90df04822
bobby29831 <bobby29831@gmail.com>
2022-02-19 17:46:34 -0600

    Initial Commit

---
+++ src/main/java/net/ecoporium/mc/MCStatus.java
@@ -0,0 +1,44 @@
+package net.ecoporium.mc;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MCStatus {
+
+    public static void main(String[] args) {
+        String baseUrl = "https://api.mcsrvstat.us/2/{ip}";
+        String ip = args[0];
+
+        if (ip.equals("")) {
+            System.out.println("Please enter a valid IP address.");
+            return;
+        }
+
+        JsonObject object = null;
+
+        try {
+            URL url = new URL(baseUrl.replace("{ip}", ip));
+            URLConnection connection = url.openConnection();
+            connection.connect();
+
+            JsonElement element = JsonParser.parseReader(new InputStreamReader((InputStream) connection.getContent()));
+            object = element.getAsJsonObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (object == null) return;
+
+        int online = object.get("players").getAsJsonObject().get("online").getAsInt();
+        int max = object.get("players").getAsJsonObject().get("max").getAsInt();
+        System.out.println("Server Status: " + object.get("online").getAsBoolean() + " (" + online + "/" + max + ")");
+    }
+
+}
